9e4a2f34a0c1df0a97a0debdd2dfd8e047be012a
Théo <boostmacom@gmail.com>
2021-05-15 15:37:24 +0200

    Ajoutez des fichiers projet.

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            List<Article> lesArticles = new List<Article>()
+            {
+                new Article(){Title = "Un article", Content = "Son contenu"},
+                new Article(){Title = "Un article", Content = "Son contenu"},
+                new Article(){Title = "Un article", Content = "Son contenu"},
+                new Article(){Title = "Un article", Content = "Son contenu"},
+                new Article(){Title = "Un article", Content = "Son contenu"}
+            };
+
+            ViewBag.Articles = lesArticles;
+            return View();
+        }
+    }
+}
---
+++ Models/Article.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Article
+    {
+        private string title;
+        private string content;
+     
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+
+    }
+}
